ace46efbd5a01905ce98824dc97e9d0c0fd0b526
IhsanSonz <mnurihsanmutaqin66@gmail.com>
2023-03-11 11:00:49 +0700

    feat: add pertemuan 5

---
+++ pboPertemuan5/src/main/java/com/sonzusb/pbopertemuan5/Universitas.java
@@ -0,0 +1,28 @@
+package com.sonzusb.pbopertemuan5;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ihsan
+ */
+public class Universitas {
+    private String nama;
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
